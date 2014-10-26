d2cb00bfa5430f4c5e1e748a8155df19b5ac2635
satwikkottur <satwik27@gmail.com>
2014-10-26 02:33:12 -0400

    Dumped the simple files using modified swt; awesome job bhayya! \m/

--- src/script.m
+++ src/script.m
@@ -1,7 +1,8 @@
 imgId = 9;
 image = imread(sprintf('../images/rectangles/%02d.png', imgId));
 
+signImg = imread('../images/testImage.jpg');
 %signImg = imread('../images/signBoard.jpg');
-%swtImg = swtransform(signImg);
-swtImg = swtransform(image(:, :, [1 1 1]));
+swtImg = swtransform(signImg);
+%swtImg = swtransform(image(:, :, [1 1 1]));
 figure; imagesc(swtImg);
