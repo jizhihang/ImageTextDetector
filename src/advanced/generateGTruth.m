51efead897fa12884bca02813e5bf59b18cd52a3
Vishwanath S R V <vishwa.hyd@gmail.com>
2014-11-27 18:21:07 -0500

    Revert "Revert "Fixed dimension exceeds error""
    This reverts commit 5323b0f7cd85a18e6499fcc9c0cf980e48349e4a.

--- src/advanced/generateGTruth.m
+++ src/advanced/generateGTruth.m
@@ -27,85 +27,89 @@ for i = 1:noImgs
 
     % Checking if the file is empty and skipping it if it is
     fileData = dir(trainLabelPath);
-    if(fileData.bytes == 0)
-        continue;
-    end
 
-    labels = dlmread(trainLabelPath);
+    if(fileData.bytes ~= 0)
+        labels = dlmread(trainLabelPath);
 
-    % Visualizing the bounding boxes 
-    ptMarker = vision.MarkerInserter('Shape','Circle','Fill', true, 'Size', 5, ...
-                    'FillColor', 'Custom', 'CustomFillColor', uint8([255 0 0]));
+        % Visualizing the bounding boxes 
+        ptMarker = vision.MarkerInserter('Shape','Circle','Fill', true, 'Size', 5, ...
+                        'FillColor', 'Custom', 'CustomFillColor', uint8([255 0 0]));
 
-    centerMarker = vision.MarkerInserter('Shape','Circle','Fill', true, 'Size', 5, ...
-                    'FillColor', 'Custom', 'CustomFillColor', uint8([0 0 255]));
-    
-    drawImage = image;
-
-    % Trasforming the points
-    centerPts = labels(:, 3:4)  + labels(:, 5:6) * 0.5;
-
-    drawPts = [];
-    tic;
-    for j = 1:size(centerPts, 1)
-        % Angle of rotation
-        angle =  -1 * labels(j, 7);
-        % Rotation matrix
-        R = [cos(angle), sin(angle); -sin(angle), cos(angle)];
-
-        centerPt = centerPts(j, :);
-        shifts = 0.5 * labels(j, 5:6);
-        % Getting four corners of the cannonical box
-        corners = [[1, 1] .* shifts; [-1, 1] .* shifts; ...
-                   [-1 -1] .* shifts; [1 -1].*shifts];  
+        centerMarker = vision.MarkerInserter('Shape','Circle','Fill', true, 'Size', 5, ...
+                        'FillColor', 'Custom', 'CustomFillColor', uint8([0 0 255]));
         
-        % Rotating the corners and translate to the center
-        corners = bsxfun(@plus, (R * corners')',  centerPt);
-        
-        if(visualize)
-            % Drawing the points
-            drawPts = [drawPts; corners];
+        drawImage = image;
+
+        % Trasforming the points
+        centerPts = labels(:, 3:4)  + labels(:, 5:6) * 0.5;
+
+        drawPts = [];
+        tic;
+        for j = 1:size(centerPts, 1)
+            % Angle of rotation
+            angle =  -1 * labels(j, 7);
+            % Rotation matrix
+            R = [cos(angle), sin(angle); -sin(angle), cos(angle)];
+
+            centerPt = centerPts(j, :);
+            shifts = 0.5 * labels(j, 5:6);
+            % Getting four corners of the cannonical box
+            corners = [[1, 1] .* shifts; [-1, 1] .* shifts; ...
+                       [-1 -1] .* shifts; [1 -1].*shifts];  
+            
+            % Rotating the corners and translate to the center
+            corners = bsxfun(@plus, (R * corners')',  centerPt);
+            
+            if(visualize)
+                % Drawing the points
+                drawPts = [drawPts; corners];
+            end
+
+            % Bounding box for the component (minRow, maxRow, minCol, maxCol) format
+            box = floor([min(corners(:,2)), max(corners(:,2)), min(corners(:,1)), max(corners(:, 1))]);
+
+            % Clipping the bounding box co-ordinates
+            box = [max(1, box(1)), min(box(2), size(image, 1)), ...
+                    max(1, box(3)), min(box(4), size(image, 2))];
+
+            % Extracting the subimage
+            subImg = image(box(1):box(2), box(3):box(4), :);
+
+            % Get swt image, components and component features
+            swtImg = swtransform(subImg, false);
+            rawComponents = connectedComponents(swtImg, 3.2);
+            [components, bboxes] = filterComponents(swtImg, rawComponents);
+            compFeat = evaluateComponentFeatures(subImg, swtImg, components, bboxes);
+
+            % Saving the component and its features for a text
+            % Identifier
+            textline{i}.imageName = trainImgs(i).name;
+            textline{i}.id = i;
+
+            textline{i}.swtImg = swtImg;
+            textline{i}.rawComponents = rawComponents;
+            textline{i}.components = components; 
+            textline{i}.bboxes = bboxes;
+            textline{i}.compFeat = compFeat;
+
+            % Saving the component features
+            trainingData{i}{count(i)} = textline{i};
+            count(i) = count(i) + 1;
+            %figure(1); imagesc(components)
+            %figure(2); imagesc(rawComponents)
+            %pause(1)
         end
 
-        % Bounding box for the component (minRow, maxRow, minCol, maxCol) format
-        box = ceil([min(corners(:,2)), max(corners(:,2)), min(corners(:,1)), max(corners(:, 1))]);
-        % Extracting the subimage
-        subImg = image(box(1):box(2), box(3):box(4), :);
+        time= toc;
+        fprintf('Training : %d / %d in %f sec\n', i, noImgs, time);
 
-        % Get swt image, components and component features
-        swtImg = swtransform(subImg, false);
-        rawComponents = connectedComponents(swtImg, 3.2);
-        [components, bboxes] = filterComponents(swtImg, rawComponents);
-        compFeat = evaluateComponentFeatures(subImg, swtImg, components, bboxes);
+        if visualize
+            drawImage = step(centerMarker, drawImage, uint32(centerPts));
+            drawImage = step(ptMarker, drawImage, uint32(drawPts));
 
-        % Saving the component and its features for a text
-        % Identifier
-        textline{i}.imageName = trainImgs(i).name;
-        textline{i}.id = i;
-
-        textline{i}.swtImg = swtImg;
-        textline{i}.rawComponents = rawComponents;
-        textline{i}.components = components; 
-        textline{i}.bboxes = bboxes;
-        textline{i}.compFeat = compFeat;
-
-        % Saving the component features
-        trainingData{i}{count(i)} = textline{i};
-        count(i) = count(i) + 1;
-        %figure(1); imagesc(components)
-        %figure(2); imagesc(rawComponents)
-        %pause(1)
-    end
-
-    time= toc;
-    fprintf('Training : %d / %d in %f sec\n', i, noImgs, time);
-
-    if visualize
-        drawImage = step(centerMarker, drawImage, uint32(centerPts));
-        drawImage = step(ptMarker, drawImage, uint32(drawPts));
-
-        figure(2); imshow(drawImage)
-        pause(3);
+            figure(2); imshow(drawImage)
+            pause(3);
+        end
     end
 end
 
