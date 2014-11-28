7622d0c36f475ace5367bbe472a49c11f4539284
satwikkottur <satwik27@gmail.com>
2014-11-28 13:43:30 -0500

    Random forest building for chains and components done, to be integrated with features from chain

--- src/advanced/trainClassifiers.m
+++ src/advanced/trainClassifiers.m
@@ -14,7 +14,7 @@
     % componentModel = Random forest classifier for components
     % chainModel = Random forest classifier for chains
     
-    trainingPath = '../../../'; noTrees = 200; imagePath = '../../MSRA-TD500/train';
+    trainingPath = '.'; noTrees = 200; imagePath = '../../MSRA-TD500/train';
     
     % Loading the dumped data for training
     load(fullfile(trainingPath, 'trainingData.mat'));
@@ -26,58 +26,73 @@
     % Component features
     % Contour shape, edge shape, occupation ratio, axial ratio, 
     % width variation, density
-    XComponents = [];
-    XChains = [];
     
     % Positive components
-    noChains = length(posData);
+    noChainsPos = length(posData);
     
-    for i = 1:noChains
+    % Negative components
+    noChainsNeg = length(negData);
+    
+    % Single pass to determine number of components, positive and negative
+    noCompsPos = 0;
+    for i = 1:noChainsPos
+        noCompsPos = noCompsPos + length(posData{i}.compFeat);
+    end
+    noCompsNeg = 0;
+    for i = 1:noChainsPos
+        noCompsNeg = noCompsNeg + length(negData{i}.compFeat);
+    end
+    
+    % 120 features for each component
+    noCompFeatures = 120;
+    XComponents = zeros(noCompsNeg + noCompsPos, noCompFeatures);
+    
+    % features for each chain
+    noChainFeatures = 11;
+    XChains = zeros(noChainsPos + noChainsNeg, noChainFeatures);
+    
+    curCompId = 1;
+    for i = 1:noChainsPos
         % Each chain, get features for components
         for j = 1:length(posData{i}.compFeat)
             curComp = posData{i}.compFeat{j};
             % Extracting current component features
-            feature = [curComp.contourShape, curComp.edgeShape, ...
+            XComponents(curCompId, :) = [curComp.contourShape, curComp.edgeShape, ...
                         curComp.occupationRatio, curComp.AxialRatio, ...
                         curComp.widthVariation, curComp.density];
             
-            % Adding it to the pool
-            XComponents = [XComponents; feature];
+            % Incrementing the component count
+            curCompId = curCompId + 1;
         end
     end
     
     % Constructing the YComponents
-    YComponents = ones(size(XComponents, 1), 1);
+    YComponents = ones(curCompId - 1, 1);
     
-    % Negative components
-    noChains = length(negData);
     
-    for i = 1:noChains
+    for i = 1:noChainsNeg
         % Reading the corresponding part of the 
         
         % Each chain, get features for components
         for j = 1:length(negData{i}.compFeat)
             curComp = negData{i}.compFeat{j};
             % Extracting current component features
-            feature = [curComp.contourShape, curComp.edgeShape, ...
+            XComponents(curCompId, :) = [curComp.contourShape, curComp.edgeShape, ...
                         curComp.occupationRatio, curComp.AxialRatio, ...
                         curComp.widthVariation, curComp.density];
             
-            % Adding it to the pool
-            XComponents = [XComponents; feature];
+            % Incrementing the component count
+            curCompId = curCompId + 1;
         end
     end
     
     % Constructing the YComponents
-    YComponents = [YComponents; zeros(size(XComponents, 1)-length(YComponents), 1)];
+    YComponents = [YComponents; zeros(curCompId - 1 -length(YComponents), 1)];
     
     fprintf('Training %d trees for the component forests\n', noTrees);
     % Train the models
     componentModel = TreeBagger(noTrees, XComponents, YComponents, ...
                     'Method', 'Classification');
-    
-    % Building the chain classifier
-    fprintf('Training %d trees for the chain forests\n', noTrees);
     
     % Getting probabilities for all the training data
     %trainingProb = zeros(length(YComponents), 2);
@@ -88,33 +103,73 @@
     fprintf('Training on components completed, accuracy : %f\n', ...
             sum(1 - xor(trainingLabel, YComponents))/length(trainingLabel));
     
-    return
-    
     % Chain features
-    % Contour shape, edge shape, occupation ratio, axial ratio, 
-    % width variation, density
-    XChains = [];
+    % Candidate count, average probability, average turning angle
+    % size variation, distance variation, average direction bias,
+    % average axial ratio, average density, average width variation, 
+    % average color self similarity, average structure self-similarity
     
-    % Positive components
-    noChains = length(posData);
+    % Positive chains
+    compCount = 1;
     
-    for i = 1:noChains
+    for i = 1:noChainsPos
         % Reading the corresponding image
-        image = imread();
-        % Each chain, get features for components
-        for j = 1:length(posData{i}.compFeat)
-            curComp = posData{i}.compFeat{j};
-            % Extracting current component features
-            feature = [curComp.contourShape, curComp.edgeShape, ...
-                        curComp.occupationRatio, curComp.AxialRatio, ...
-                        curComp.widthVariation, curComp.density];
-            
-            % Adding it to the pool
-            XComponents = [XComponents; feature];
-        end
+        image = imread(fullfile(imagePath, posData{i}.imageName));
+        box = posData{i}.bboxes;
+        
+        % Evaluation of chain features
+        % Takes in image, components, chains, compFeat, compProbabilities
+        iterImage = image(box(1) : box(2), box(3):box(4), :);
+        iterComponents = posData{i}.components;
+        noComps = length(iterComponents);
+        iterChains = 1:noComps;
+        iterCompFeat = posData{i}.compFeat;
+        iterProbabilities = trainingProb(compCount:(compCount + noComps -1), 2);
+        compCount = compCount + noComps;
+        
+        feature = evaluateChainFeatures(iterImage, iterComponents, ...
+                        iterChains, iterCompFeat, iterProbabilities);
+
+        % Adding it to the pool
+        XChains(i, :) = feature;
     end               
     
-    chainModel = 1;
+    % Y for these chains
+    YChains = ones(noChainsPos, 1);
+    
+    % Negative chains picked up from negative examples
+     
+    for i = 1:noChainsNeg
+        % Reading the corresponding image
+        image = imread(fullfile(imagePath, negData{i}.imageName));
+        box = negData{i}.bboxes;
+        
+        % Evaluation of chain features
+        % Takes in image, components, chains, compFeat, compProbabilities
+        iterImage = image(box(1) : box(2), box(3):box(4), :);
+        iterComponents = negData{i}.components;
+        noComps = length(iterComponents);
+        iterChains = 1:noComps;
+        iterCompFeat = negData{i}.compFeat;
+        iterProbabilities = trainingProb(compCount:(compCount + noComps -1), 2);
+        compCount = compCount + noComps;
+        
+        feature = evaluateChainFeatures(iterImage, iterComponents, ...
+                        iterChains, iterCompFeat, iterProbabilities);
+
+        % Adding it to the pool
+        XChains(i, :) = feature;
+    end
+    
+    % Updating the Y of chains
+    YChains = [YChains; -1 * ones(noChainsNeg, 1)];
+   
+    % Building the chain classifier
+    fprintf('Training %d trees for the chain forests\n', noTrees);
+    
+    % Training the random forest
+    chainModel = TreeBagger(noTrees, XChains, YChains, ...
+                    'Method', 'Classification');
     
 %end
 
